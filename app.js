7e9717c58be32c1843bf3155688996ca3a7c8897
Alexis Lujan Oruna <alujan@ngr.com.pe>
2024-12-13 08:52:22 -0500

    Firsts

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ app.post('/upload', upload.single('file'), (req, res) => {
     // Devuelve la URL para descargar el archivo procesado
     res.json({
       message: 'Archivo procesado exitosamente.',
-      downloadUrl: `http://localhost:${PORT}/processed/archivo_procesado.xlsx`,
+      downloadUrl: `https://proyect-w8sl.onrender.com:${PORT}/processed/archivo_procesado.xlsx`,
     });
   } catch (error) {
     console.error(error.message);
